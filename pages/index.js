26181f53a9ee3b6b1ccb48a51a6a16709bf7ec26
Jorge Wilkerson <will.engcomp.2014@gmail.com>
2025-12-24 18:31:26 +0000

    install jest and configure test scripts

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Meu bem, eu te amo. Você é a mulher da minha vida. </h1>    
+  return <h1>Meu bem, eu te amo. Você é a mulher da minha vida. </h1>;
 }
 
 export default Home;
