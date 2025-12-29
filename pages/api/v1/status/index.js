6852d7f42ec6acdf50799ceb9d0712476bd16c0f
Jorge Wilkerson <will.engcomp.2014@gmail.com>
2025-12-29 18:49:48 +0000

    add `database.js`and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import databese from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await databese.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response.status(200).json({ chave: "it's working" });
 }
 
