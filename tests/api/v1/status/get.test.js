361fafc6d0b08be22eac0a5f529c73ca9773aa0b
Jorge Wilkerson <will.engcomp.2014@gmail.com>
2025-12-31 14:07:27 +0000

    finish `/api/v1/status` endpoint

--- tests/api/v1/status/get.test.js
+++ tests/api/v1/status/get.test.js
@@ -2,4 +2,14 @@ test("GET to api/v1/status should return 200", async () => {
   // await soh funciona com funcao assincrona -> async
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
 });
